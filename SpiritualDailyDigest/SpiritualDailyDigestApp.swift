95851971760a04a2c0ca8f70ba7c7aec8e9b62e0
Bello Abraham <belloabraham101@gmail.com>
2023-08-14 20:30:36 +0100

    Initial Commit

---
+++ SpiritualDailyDigest/SpiritualDailyDigestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiritualDailyDigestApp.swift
+//  SpiritualDailyDigest
+//
+//  Created by Bello Abraham on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SpiritualDailyDigestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
